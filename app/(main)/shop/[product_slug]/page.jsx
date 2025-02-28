1561e1dd301debac9ef6f4c4caa86115d3c46783
djalilsel <selamnia.a.eldjalil@gmail.com>
2025-02-28 18:39:00 +0100

    /shop /shop/:product_slug api fetch

--- app/(main)/shop/[product_slug]/page.jsx
+++ app/(main)/shop/[product_slug]/page.jsx
@@ -1,18 +1,17 @@
 "use client";
 
 import useFetchData from "@/hooks/useFetchData";
+import { useParams } from "next/navigation";
 
 const page = () => {
-  const page_number = 1;
-  const page_size = 10;
+  const { product_slug } = useParams();
   const { data, loading, error } = useFetchData(
-    process.env.NEXT_PUBLIC_BACKEND_URL +
-      `/products?populate=*&pagination[page]=${page_number}&pagination[pageSize]=${page_size}`
+    process.env.NEXT_PUBLIC_BACKEND_URL + `/products/${product_slug}`
   );
   console.log(data, loading, error);
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
-  return <div>shop</div>;
+  return <div>Product page</div>;
 };
 
 export default page;
